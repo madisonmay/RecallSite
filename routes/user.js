aa9f8699cdc3692ae38bae3ddf1432f07890ac95
madisonmay13 <madison.may@students.olin.edu>
2013-03-24 15:33:46 -0400

    Adding querystring back in

--- routes/user.js
+++ routes/user.js
@@ -60,7 +60,7 @@ exports.connect = function(req, res) {
 
 exports.fb_search = function(req, res) {
     console.log(req.query.q)
-    var url = '/me/home?' + 'limit=200';
+    var url = '/me/home?q=' + req.query.q + '&fields=from,message';
     console.log(url);
     req.facebook.api(url, function(err, data) {
         console.log(err);
